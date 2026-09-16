fe534e4fb766d40cd4dff09c46340d1ed2e9916e
sharvarimodi3 <sharvariwork03@gmail.com>
2026-09-16 23:04:37 +0530

    done

--- looping2.py
+++ looping2.py
@@ -1,8 +1,8 @@
 correct_pass = "some_pass"
- not_found = True
+not_found = True
 
- while not_found:
+while not_found:
      passw = input("Enter pass: ")
      if passw == correct_pass:   #breaking condition
          not_found = False
- print("Password Matched!")
+print("Password Matched!")
---
+++ third.py
@@ -0,0 +1,6 @@
+list = [10, 20, 30, 50, 50]
+sum = 0
+for num in list:
+    sum = sum + num
+    average = sum / len(list)
+print("The average of the given list is: ", average)
